f7a12c0341bbdcc877d650096cac684adddb6f16
YoonJee00 <bestjang00@gmail.com>
2023-03-23 15:49:41 +0900

    유효성 검사

---
+++ src/main/java/com/together/handler/aop/ValidationAdvice.java
@@ -0,0 +1,59 @@
+package com.together.handler.aop;
+
+import com.together.handler.ex.CustomValidationApiException;
+import com.together.handler.ex.CustomValidationException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+public class ValidationAdvice {
+    @Around("execution(* com.together.web.api.*Controller.*(..))")
+    public Object apiAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        //System.out.println("web api controller ======================");
+        Object[] args = proceedingJoinPoint.getArgs();
+        for(Object arg: args) {
+            if(arg instanceof BindingResult) {
+                //System.out.println("유효성 검사를 하는 함수입니다.");
+                BindingResult bindingResult = (BindingResult) arg;
+
+                if(bindingResult.hasErrors()) {
+                    Map<String, String> errorMap = new HashMap<>();
+
+                    for(FieldError error:bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                    }
+                    throw new CustomValidationApiException("유효성검사 실패", errorMap);
+                }
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
+
+    @Around("execution(* com.together.web.*Controller.*(..))")
+    public Object advice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        //System.out.println("web controller ======================");
+        Object[] args = proceedingJoinPoint.getArgs();
+        for(Object arg: args) {
+            if(arg instanceof BindingResult) {
+                BindingResult bindingResult = (BindingResult) arg;
+                if(bindingResult.hasErrors()) {
+                    Map<String, String> errorMap = new HashMap<>();
+
+                    for(FieldError error : bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                    }
+                    throw new CustomValidationException("유효성검사 실패", errorMap);
+                }
+            }
+        }
+        return proceedingJoinPoint.proceed();
+    }
+}
