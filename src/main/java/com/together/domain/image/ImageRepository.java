64fc2d35c4aa865bcf6c582ce26335def0db2d31
YoonJee00 <bestjang00@gmail.com>
2023-03-30 12:26:06 +0900

    스토리 페이지에서 자신의 글도 볼 수 있도록 바꿈

--- src/main/java/com/together/domain/image/ImageRepository.java
+++ src/main/java/com/together/domain/image/ImageRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
-    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) OR userId = :principalId ORDER BY id DESC", nativeQuery = true)
     Page<Image> mStory(int principalId, Pageable pageable);
 
     @Query(value = "SELECT i.* FROM image i INNER JOIN (SELECT imageId, COUNT(imageId) likeCount FROM likes GROUP BY imageId) c ON i.id = c.imageId ORDER BY likeCount DESC", nativeQuery = true)
@@ -17,9 +17,9 @@ public interface ImageRepository extends JpaRepository<Image, Integer> {
 
     void deleteAllByUserId(int userId);
 
-    @Query(value="SELECT id, caption, postImageUrl,createDate,userId, \r\n"
-            + "(Select COUNT(*) from likes where imageId = :imageId)likeCount,\r\n"
-            + "(Select EXISTS (select id from likes where userId = :principalId and imageId = :imageId))likeState\r\n"
-            + "FROM image where id = :imageId", nativeQuery = true)
+    @Query(value = "SELECT id, caption, postImageUrl, createDate, userId, \r\n"
+            + "(SELECT COUNT(*) from likes where imageId = :imageId)likeCount, \r\n"
+            + "(SELECT EXISTS (SELECT id FROM likes WHERE userId = :principalId and imageId = :imageId))likeState\r\n"
+            + "FROM image WHERE id = :imageId", nativeQuery = true)
     Image imageDetail(@Param("imageId") int imageId, @Param("principalId") int principalId);
 }
